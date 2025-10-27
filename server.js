eb2db8bc68e90e03228fdea500a5aba7678c558e
Ahmad Khatib <ahmadkhatib29857adis@gmail.com>
2025-10-27 20:32:05 +0400

    hehheheh

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
+import express from 'express';
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 10000;
 
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname,"index.html"));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-const port = process.env.PORT || 10000;
-app.listen(port, ()=>console.log(`Server running on port ${port}`));
